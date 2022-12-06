1fb8aeea2eba96e4217efe119863196ff902c4a4
Archetipo95 <martin.masevski@gmail.com>
2022-12-06 22:37:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# svg-path-animation
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-7lax1v)
---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import Plane from './components/Plane.vue';
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  let svg = document.querySelectorAll('svg');
+
+  let paths = document.querySelectorAll('path');
+
+  fillSvgPaths();
+
+  document.addEventListener('scroll', fillSvgPaths);
+
+  function fillSvgPaths() {
+    let scrollPercentage =
+      (document.documentElement.scrollTop + document.body.scrollTop) /
+      (document.documentElement.scrollHeight -
+        document.documentElement.clientHeight);
+
+    for (var i = 0; i < paths.length; i++) {
+      let path = paths[i];
+
+      let pathLength = path.getTotalLength();
+
+      path.style.strokeDasharray = pathLength;
+      path.style.strokeDashoffset = pathLength;
+
+      let drawLength = pathLength * scrollPercentage;
+
+      path.style.strokeDashoffset = pathLength - drawLength;
+    }
+  }
+});
+</script>
+
+<template>
+  <section>
+    <h1>eheh</h1>
+  </section>
+
+  <section>
+    <h1>They go so fast</h1>
+  </section>
+
+  <section>
+    <h1>EHEH</h1>
+  </section>
+
+  <Plane />
+</template>
+
+<style scoped></style>
